c447fc2e8de93cf5e1597556b9702fc65504ce6f
Nayanathara Dhanasekara <nayanathara@4axissolutions.com>
2022-02-11 16:49:16 +0530

    second commit

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -51,5 +51,10 @@ class ConverterTests: XCTestCase {
     let result = converter.convert(2)
     XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
   }
+    
+    func testConversionForTwo() {
+      let result = converter.convert(2)
+      XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
+    }
   
 }
